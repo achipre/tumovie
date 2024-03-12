0067d077acd7903ef9eed41b5366c1a5a8ef75a6
Alex Chipre <alechipre@gmail.com>
2024-03-11 20:02:39 -0500

    add Card and Search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,46 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import { AddReview } from './components/add-review'
+import { Login } from './components/login'
+import { Movie } from './components/movie'
+import { MoviesList } from './components/movies-list'
 
 export const App = () => {
+  const [user, setUser] = useState(null)
+
+  async function login (user = null) {
+    setUser(user)
+  }
+
+  async function logout () {
+    setUser(null)
+  }
+
   return (
-    <nav className="bg-white border-gray-200 dark:bg-[#F05454]">
-     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-       <Link
-        to={'/'}
-        className="flex items-center space-x-3 rtl:space-x-reverse"
-      >
-        <img src="/favicon.svg" className="h-8" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-[#30475E]">
-          Tus Pelis
-        </span>
-      </Link>
-      <div className="flex">
-        <button
-          type="button"
-          data-collapse-toggle="navbar-search"
-          aria-controls="navbar-search"
-          aria-expanded="false"
-          className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none  rounded-lg text-sm p-2.5 me-1 focus-within:outline-none"
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-[#F05454]">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to={'/'}
+          className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            fill="#FFC3A1"
-            viewBox="0 0 20 20"
+          <img src="/favicon.svg" className="h-8" alt="Flowbite Logo" />
+          <h1 className="self-center text-2xl font-semibold whitespace-nowrap dark:text-[#30475E]">
+            Tus Pelis
+          </h1>
+        </Link>
+        <div className="flex">
+          <button
+            type="button"
+            data-collapse-toggle="navbar-search"
+            aria-controls="navbar-search"
+            aria-expanded="false"
+            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none  rounded-lg text-sm p-2.5 me-1 focus-within:outline-none"
           >
-            <path
-              stroke="#FFC3A1"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-          <span className="sr-only">Search</span>
-        </button>
-        <div className="relative hidden md:block">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              className="w-5 h-5"
               aria-hidden="true"
-              fill="none"
+              fill="#FFC3A1"
               viewBox="0 0 20 20"
             >
               <path
@@ -53,88 +51,121 @@ export const App = () => {
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
-            <span className="sr-only">Search icon</span>
-          </div>
-          <input
-            type="text"
-            id="search-navbar"
-            className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-[#FFC3A1] dark:text-white focus-visible:outline-none"
-            placeholder="Search..."
-          />
-        </div>
-        <button
-          data-collapse-toggle="navbar-search"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-search"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            fill="#FFC3A1"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="#FFC3A1"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
+            <span className="sr-only">Search</span>
+          </button>
+          <div className="relative hidden md:block">
+            <div className="absolute hidden inset-y-0 start-0 items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="#FFC3A1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Search icon</span>
+            </div>
+            <input
+              type="text"
+              id="search-navbar"
+              className="hidden w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-[#FFC3A1] dark:text-white focus-visible:outline-none"
+              placeholder="Search..."
             />
-          </svg>
-        </button>
-      </div>
-      {/* TODO: Hidden */}
-      <div
-        className="items-center justify-between w-full hidden md:flex md:w-auto md:order-1"
-        id="navbar-search"
-      >
-        <div className="relative mt-3 md:hidden">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          </div>
+          <button
+            data-collapse-toggle="navbar-search"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-search"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
             <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              className="w-5 h-5"
               aria-hidden="true"
-              fill="none"
-              viewBox="0 0 20 20"
+              fill="#FFC3A1"
+              viewBox="0 0 17 14"
             >
               <path
                 stroke="#FFC3A1"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
-          </div>
-          <input
-            type="text"
-            id="search-navbar"
-            className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-[#FFC3A1] dark:text-white focus-within:outline-none"
-            placeholder="Search..."
-          />
+          </button>
         </div>
-        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-[transparent] dark:border-gray-700">
-          <li>
-            <Link
-              to="/movie"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-[#30475E] dark:text-[#FFC3A1] dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Movies
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/login"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-[#FFC3A1] md:dark:hover:text-[#30475E] dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Login
-            </Link>
-          </li>
-        </ul>
+        {/* TODO: Hidden */}
+        <div
+          className="items-center justify-between w-full md:flex md:w-auto md:order-1"
+          id="navbar-search"
+        >
+          <div className="relative mt-3 md:hidden">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="#FFC3A1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="search-navbar"
+              className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-[#FFC3A1] dark:text-white focus-within:outline-none"
+              placeholder="Search..."
+            />
+          </div>
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-[transparent] dark:border-gray-700">
+            <li>
+              <Link
+                to="/movies"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:text-[#30475E] dark:hover:text-[#FFC3A1] text-[#FFC3A1] dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Movies
+              </Link>
+            </li>
+            <li>
+              {
+                user
+                  ? <a href='#' onClick={logout}
+                className="block py-2 px-3 text-[#FFC3A1] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-[#FFC3A1] md:dark:hover:text-[#30475E] dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
+                >Logout</a>
+                  : <Link
+                to="/login" onClick={login}
+                className="block py-2 px-3 text-[#FFC3A1] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:hover:text-[#FFC3A1] md:dark:text-[#30475E] dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Login
+              </Link>
+              }
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+      </nav>
+      <Routes>
+        <Route path='/' element={<MoviesList />} />
+        <Route path='/movies' element={<MoviesList />} />
+        <Route path='/movies/:id' element={<Movie />} />
+        <Route path='/movies/:id/review' element={<AddReview user={user} />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+   </>
+
   )
 }
--- frontend/src/components/movies-list.jsx
+++ frontend/src/components/movies-list.jsx
@@ -1,10 +1,166 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { MovieDateService } from '../services/movies'
 
 export const MoviesList = () => {
+  const [categoryVisible, setCategoryVisible] = useState(false)
+
+  const openCategory = (e) => {
+    setCategoryVisible(!categoryVisible)
+  }
+  const closeCategory = (e) => {
+    setCategoryVisible(!categoryVisible)
+    setSearchRating(e.target.textContent)
+  }
+
+  const [movies, setMovies] = useState([])
+  const [searchTitle, setSearchTitle] = useState('')
+  const [searchRating, setSearchRating] = useState('All categories')
+  const [ratings, setRatings] = useState(['All categories'])
+
+  useEffect(() => {
+    retriveMovies()
+    retriveRatingss()
+  }, [])
+
+  const retriveMovies = () => {
+    MovieDateService.getAll()
+      .then((resp) => {
+        setMovies(resp.data.movies)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+  const retriveRatingss = () => {
+    MovieDateService.getRatings()
+      .then((resp) => {
+        setRatings(['All categories'].concat(resp.data))
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value
+    setSearchTitle(searchTitle)
+  }
+
+  console.log(movies)
+
+  console.log(searchTitle)
   return (
     <>
-      <Link to={'/'}>Home</Link>
-      <div>MoviesList</div>
+      <form className="max-w-lg mx-auto">
+        <div className="flex">
+          <div className='relative'>
+            <button
+              onClick={openCategory}
+              id="dropdown-button"
+              data-dropdown-toggle="dropdown"
+              className="min-w-36 flex-shrink-0 z-10 inline-flex justify-between items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-[#FFC3A1] dark:border-gray-600 text-nowrap"
+              type="button"
+            >
+              <span>
+                {searchRating}
+              </span>
+              <svg
+                className="w-2.5 h-2.5 ms-2.5"
+                aria-hidden="true"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+            <div
+              id="dropdown"
+              className={`z-10 absolute top-0 ${categoryVisible ? '' : 'hidden'} bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 w-full`}
+            >
+              <ul
+                className="py-2 text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdown-button"
+              >
+                {
+                  ratings.map((rating, idx) => (
+                  <li key={idx}>
+                    <button
+                      type="button"
+                      onClick={closeCategory}
+                      className="inline-flex w-full text-nowrap px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-[#FFC3A1] text-sm font-medium"
+                    >
+                      {rating}
+                    </button>
+                  </li>
+                  ))
+                }
+              </ul>
+            </div>
+          </div>
+          <div className="relative w-full">
+            <input
+              type="search"
+              id="search-dropdown"
+              onChange={onChangeSearchTitle}
+              className="block p-2.5 w-full z-20 text-sm text-white bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300  dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 focus-visible:outline-none placeholder:dark:text-[#FFC3A1]"
+              placeholder="Batman..."
+              required
+            />
+            <button
+              type="submit"
+              className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white dark:bg-[#F05454] rounded-e-lg dark:hover:bg-[#f05454df]"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                viewBox="0 0 20 20"
+                fill="none"
+              >
+                <path
+                  stroke="#FFC3A1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Search</span>
+            </button>
+          </div>
+        </div>
+      </form>
+      <main className='w-full mx-auto max-w-7xl p-4 flex flex-wrap gap-3 justify-between'>
+      {
+
+        movies.map(movie => (
+          <div key={movie._id} className="flex flex-col w-[19%]  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <img className="rounded-t-lg aspect-[0.62] object-cover w-full" src={movie.poster || '/imgNotFound.jpeg'} alt="" />
+            <div className="p-5">
+              <a href="#">
+                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h2>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{movie.plot}</p>
+              <a href="#" className="flex gap-2 mt-auto justify-center flex-grow items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none  dark:bg-blue-600 dark:hover:bg-blue-700 focus-visible:outline-none">
+                <span>
+                  View Review
+                </span>
+                <svg className="w-3.5 h-3.5" aria-hidden="true" fill="none" viewBox="0 0 14 10">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        ))
+
+      }
+      </main>
+
     </>
   )
 }
