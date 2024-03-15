39eb2e636adfec4a14098b4e42efbc80b8a1cbad
Alex Chipre <alechipre@gmail.com>
2024-03-15 15:15:59 -0500

    Change Service Data Service

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -2,31 +2,31 @@ import axios from 'axios'
 
 export class MovieDateService {
   static getAll (page = 0) {
-    return axios.get(`http://localhost:5050/api/v1/movies?page=${page}`)
+    return axios.get(`https://tumovie.onrender.com/api/v1/movies?page=${page}`)
   }
 
   static get (id) {
-    return axios.get(`http://localhost:5050/api/v1/movies/id/${id}`)
+    return axios.get(`https://tumovie.onrender.com/api/v1/movies/id/${id}`)
   }
 
   static find (queryRated = '', queryTitle = '', page = 0) {
-    return axios.get(`http://localhost:5050/api/v1/movies?${queryRated}${queryTitle}&page=${page}`)
+    return axios.get(`https://tumovie.onrender.com/api/v1/movies?${queryRated}${queryTitle}&page=${page}`)
   }
 
   static createReview (data) {
-    return axios.post('http://localhost:5050/api/v1/movies/review', data)
+    return axios.post('https://tumovie.onrender.com/api/v1/movies/review', data)
   }
 
   static upadteReview (data) {
     console.log(data)
-    return axios.put('http://localhost:5050/api/v1/movies/review', data)
+    return axios.put('https://tumovie.onrender.com/api/v1/movies/review', data)
   }
 
   static deleteReview (id, userId) {
-    return axios.delete('http://localhost:5050/api/v1/movies/review', { data: { review_id: id, user_id: userId } })
+    return axios.delete('https://tumovie.onrender.com/api/v1/movies/review', { data: { review_id: id, user_id: userId } })
   }
 
   static getRatings () {
-    return axios.get('http://localhost:5050/api/v1/movies/ratings')
+    return axios.get('https://tumovie.onrender.com/api/v1/movies/ratings')
   }
 }
